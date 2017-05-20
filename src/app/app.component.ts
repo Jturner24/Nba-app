207ac43dbdd5c5307de9273154fff768a16dddac
Jordan Turner <jt9514728371@gmail.com>
2017-05-19 18:37:00 -0700

    progresed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,8 +21,8 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Stats', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Home', component: HomePage },
+      { title: 'Teams', component: ListPage }
     ];
 
   }
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -8,7 +8,8 @@ import { NavController, NavParams } from 'ionic-angular';
 export class ListPage {
   selectedItem: any;
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  items: Array<{ title: string, icon: string }>;
+  teams: Array<{ name: string, id: string }>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     // If we navigated to this page, we will have an item available as a nav param
@@ -16,16 +17,96 @@ export class ListPage {
 
     // Let's populate this page with some filler content for funzies
     this.icons = ['paper-plane', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+      'american-football', 'boat', 'bluetooth', 'build'];
 
     this.items = [];
-    for (let i = 1; i < 11; i++) {
+    for (let i = 1; i < 33; i++) {
       this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
+        title: 'lakers ' + i,
+
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
     }
+
+    this.teams = [
+      {
+        name: 'Atlanta Hawks',
+        id: "1610612737"
+      },
+      {
+        name: 'Boston Celtics',
+        id: "1610612738"
+      },
+      {
+        name: 'Brooklyn Nets',
+        id: "1610612751"
+      },
+      {
+        name: '',
+        id: ""
+      },
+      {
+        name: '',
+        id: ""
+      },
+      {
+        name: '',
+        id: ""
+      },
+      {
+        name: '',
+        id: ""
+      },
+      {
+        name: '',
+        id: ""
+      },
+      {
+        name: '',
+        id: ""
+      },
+      {
+
+        name: '',
+        id: ""
+      },
+      {
+        name: '',
+        id: ""
+      },
+      {
+        name: '',
+        id: ""
+      },
+      {
+        name: '',
+        id: ""
+      },
+      {
+        name: '',
+        id: ""
+      },
+      {
+        name: '',
+        id: ""
+      },
+      {
+        name: '',
+        id: ""
+      },
+      {
+        name: '',
+        id: ""
+      },
+      {
+        name: '',
+        id: ""
+      },
+      {
+        name: '',
+        id: ""
+      },
+    ]
   }
 
   itemTapped(event, item) {
